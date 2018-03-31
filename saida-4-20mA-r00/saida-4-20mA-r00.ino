0c9b8fafc34e3af7df9e49abc3f753b4468633d0
Clovis <clovisf@gmail.com>
2018-03-31 15:48:17 -0300

    comentado código do Arduino

--- saida-4-20mA-r00/saida-4-20mA-r00.ino
+++ saida-4-20mA-r00/saida-4-20mA-r00.ino
@@ -1,20 +1,20 @@
 
-#define analogOutput 9    // LED connected to digital pin 9
-#define analogRef A0  // Analog input pin that the potentiometer is attached to
+#define analogOutput 9    // Saída PWM controlada pelo potenciômetro (referência para o 4-20mA)
+#define analogRef A0  // Entrada analógica lendo o potenciômetro
 
-int intOutput = 51;
-int analogRefValue = 255;
+int intOutput = 51; // valor mínimo de PWM para que a saída em corrente seja 4mA
+int analogRefValue = 0; // 
 
 void setup() {
-  analogWrite(analogOutput, intOutput);
+  analogWrite(analogOutput, intOutput); // Escreve o valor 51 (~4mA) na saída PWM (pino 9)
 }
 
 void loop() {
 
-  analogRefValue= analogRead(analogRef);
-  intOutput = map(analogRefValue, 0, 1023, 30, 255);
-  analogWrite(analogOutput, intOutput);
-  delay(100); 
+  analogRefValue= analogRead(analogRef); // faz a leitura do potenciômetro referência
+  intOutput = map(analogRefValue, 0, 1023, 30, 255); // mapeia o valor do potenciômetro, de 0-1023 para 51-255
+  analogWrite(analogOutput, intOutput); // Escreve no pino 9 o valor de PWM referente á corrente desejada (51-255 = 4-20mA)
+  delay(100); // Espera 0,1 segundos e repete tudo novamente
    
   }
 
